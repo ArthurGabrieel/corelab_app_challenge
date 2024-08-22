6c682dcb4e924fbc66f939af6f4fa316dea96462
Arthur Gabriel Lima Gomes <78980894+ArthurGabrieel@users.noreply.github.com>
2024-08-22 11:36:35 -0300

    add mock products

--- lib/shared/external/mock/list_products.dart
+++ lib/shared/external/mock/list_products.dart
@@ -3,10 +3,9 @@ part of 'products_mock.dart';
 final _products = [
   ProductModel(
     name: 'Condicionador Ácido Fosfórico 37% - AllPrime',
-    price: 100.0,
-    oldPrice: 120.0,
+    price: 29.90,
     installmentQuantity: 12,
-    installmentPrice: 10.0,
+    installmentPrice: 249.90,
     category: 'Dentística e Estética',
     image:
         'https://s3-alpha-sig.figma.com/img/3ded/87b5/07c8c009afd033ee12f5c860c659db64?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mBNAQ7Fywqdaxx5ZsR7zHmYFFiHGZB4lqkk1UqLdMs66dXFrnsXpRSaJIejrHaeQ3QlC1NdTq3gEg5Ma0cveB0Bw0GGKLy0lJLRFsC0R7pM9zvHk5gqZxr0YslENLb5KyCIqiM4IUVSXffxZ92FHyMeChyUjiRo4gKcTMjCpJVmfv~gCHnYj89ZcZuRohhrmmmfBiOFnyy6judbZCm1moeCXpjxRXwB1dU42GUiK2D1HAkIYcf8KB4LOiZIWOZjjZDClCHCnX1amMNmMje-M68H-7-Rpas8sN6FpHeDG7gMpkcQ9Y32JLP-qTqlEGFh49H-XKN3XZeA5LRT0CzEpZg__',
@@ -16,10 +15,10 @@ final _products = [
   ),
   ProductModel(
     name: 'Condicionador Ácido Fosfórico 37% - AllPrime',
-    price: 100.0,
-    oldPrice: 120.0,
+    price: 290.90,
+    oldPrice: 300.0,
     installmentQuantity: 12,
-    installmentPrice: 10.0,
+    installmentPrice: 249.90,
     category: 'Dentística e Estética',
     image:
         'https://s3-alpha-sig.figma.com/img/3ded/87b5/07c8c009afd033ee12f5c860c659db64?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mBNAQ7Fywqdaxx5ZsR7zHmYFFiHGZB4lqkk1UqLdMs66dXFrnsXpRSaJIejrHaeQ3QlC1NdTq3gEg5Ma0cveB0Bw0GGKLy0lJLRFsC0R7pM9zvHk5gqZxr0YslENLb5KyCIqiM4IUVSXffxZ92FHyMeChyUjiRo4gKcTMjCpJVmfv~gCHnYj89ZcZuRohhrmmmfBiOFnyy6judbZCm1moeCXpjxRXwB1dU42GUiK2D1HAkIYcf8KB4LOiZIWOZjjZDClCHCnX1amMNmMje-M68H-7-Rpas8sN6FpHeDG7gMpkcQ9Y32JLP-qTqlEGFh49H-XKN3XZeA5LRT0CzEpZg__',
@@ -28,14 +27,27 @@ final _products = [
     createdAt: DateTime.now(),
   ),
   ProductModel(
-    name: 'Condicionador Ácido Fosfórico 37% - AllPrime',
-    price: 100.0,
-    oldPrice: 120.0,
+    name: 'Máscara Cirúrgica Tripla com Elástico - Descarpack',
+    price: 290.90,
+    oldPrice: 300.00,
     installmentQuantity: 12,
-    installmentPrice: 10.0,
-    category: 'Dentística e Estética',
+    installmentPrice: 249.90,
+    category: 'Descatáveis',
     image:
-        'https://s3-alpha-sig.figma.com/img/3ded/87b5/07c8c009afd033ee12f5c860c659db64?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mBNAQ7Fywqdaxx5ZsR7zHmYFFiHGZB4lqkk1UqLdMs66dXFrnsXpRSaJIejrHaeQ3QlC1NdTq3gEg5Ma0cveB0Bw0GGKLy0lJLRFsC0R7pM9zvHk5gqZxr0YslENLb5KyCIqiM4IUVSXffxZ92FHyMeChyUjiRo4gKcTMjCpJVmfv~gCHnYj89ZcZuRohhrmmmfBiOFnyy6judbZCm1moeCXpjxRXwB1dU42GUiK2D1HAkIYcf8KB4LOiZIWOZjjZDClCHCnX1amMNmMje-M68H-7-Rpas8sN6FpHeDG7gMpkcQ9Y32JLP-qTqlEGFh49H-XKN3XZeA5LRT0CzEpZg__',
+        'https://s3-alpha-sig.figma.com/img/d56c/2671/cf797f31fdd77ef19dbe24818374720e?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qJ1O2xygSpYsm5ZKB6sI-r390aBYO1DvSNAUEe1OPHXihpHUrHbuEK500Wc~OmmDOb3VZJc93yVmAhn7j1u~dEBIUASEakKyS4GYuGNCRCLJyT0f6ZH1BNlLVkwXL9PcmtnHEJSyCMh~Nwm3wtnaETaj5dhmJ1t8e4diBKuoPKRG4jZ6wFGVHbiUOJk9YjHIGa0uQmt3EbnwPHkcZrcGImADzCWMz225erNBWuf5kwONqLgGA8Yy7Slnx6TjcQFuoZJrOud4OSJJdkvdfoOD--qY8VGfeLHoRdng6i2rQ2pEBFRWCCgQUe572ZciTIjL6VjOrSkzNzHYyKdW9zTzlg__',
+    discount: 10,
+    isNew: true,
+    createdAt: DateTime.now(),
+  ),
+  ProductModel(
+    name: 'Máscara Cirúrgica Tripla com Elástico - Descarpack',
+    price: 290.90,
+    oldPrice: 300.00,
+    installmentQuantity: 12,
+    installmentPrice: 249.90,
+    category: 'Descatáveis',
+    image:
+        'https://s3-alpha-sig.figma.com/img/d56c/2671/cf797f31fdd77ef19dbe24818374720e?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qJ1O2xygSpYsm5ZKB6sI-r390aBYO1DvSNAUEe1OPHXihpHUrHbuEK500Wc~OmmDOb3VZJc93yVmAhn7j1u~dEBIUASEakKyS4GYuGNCRCLJyT0f6ZH1BNlLVkwXL9PcmtnHEJSyCMh~Nwm3wtnaETaj5dhmJ1t8e4diBKuoPKRG4jZ6wFGVHbiUOJk9YjHIGa0uQmt3EbnwPHkcZrcGImADzCWMz225erNBWuf5kwONqLgGA8Yy7Slnx6TjcQFuoZJrOud4OSJJdkvdfoOD--qY8VGfeLHoRdng6i2rQ2pEBFRWCCgQUe572ZciTIjL6VjOrSkzNzHYyKdW9zTzlg__',
     discount: 10,
     isNew: true,
     createdAt: DateTime.now().subtract(const Duration(days: 1)),
--- lib/shared/external/mock/products_mock.dart
+++ lib/shared/external/mock/products_mock.dart
@@ -3,18 +3,29 @@ import 'package:corelab_app_challenge/shared/data/models/product_model.dart';
 part 'list_products.dart';
 
 class ProductsMock {
-  final _recentSearchs = ['Ácido Fosfórico', 'AllPrime'];
+  final _recentSearchs = [
+    'Ácido Fosfórico',
+    'AllPrime',
+    'Dentística',
+    'Estética',
+    '249',
+    'Máscara Cirúrgica',
+    'Descarpack',
+    'Descatáveis',
+    '290',
+  ];
 
   List<ProductModel> get products => _products;
 
   List<String> get recentsSearchs => _recentSearchs;
 
   List<ProductModel> search(String query) {
+    final queryLowerCase = query.toLowerCase();
     return _products
         .where((element) =>
-            element.name.toLowerCase().contains(query) ||
-            element.price.toString().contains(query) ||
-            element.category.toLowerCase().contains(query))
+            element.name.toLowerCase().contains(queryLowerCase) ||
+            element.price.toString().contains(queryLowerCase) ||
+            element.category.toLowerCase().contains(queryLowerCase))
         .toList();
   }
 
